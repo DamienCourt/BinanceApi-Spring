f45717f01575bfc9fe80e2fcd48b247eaa9ac9fa
George Georgiev <georgiev.main@gmail.com>
2025-01-20 09:33:39 +0200

    initial commit

---
+++ src/main/java/my/BinanceApi/BinanceApiApplication.java
@@ -0,0 +1,13 @@
+package my.BinanceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/BinanceApi/BinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package my.BinanceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
